9ac2d9d3045b6f7556fa07ba226ab1a96ef7ac14
Steven Klise <sklise@gmail.com>
2012-10-30 13:03:41 -0400

    Add Connect and sessions

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 var connect = require('connect')
+var redis = require('connect-redis')(connect)
 var debug = require('debug')('http')
 var ecstatic = require('ecstatic')(__dirname + '/public')
 var ejs = require('ejs')
+var url = require('url')
 
 var publicDir = __dirname + '/public/'
 var viewsDir = __dirname + '/views/'
 var port = process.env.PORT || 3000
+var redisUrl = url.parse(process.env.REDISTOGO_URL || 'redis://localhost:6379')
 
 debug('booting shep.info')
 
 var matchRoutes = function (req, res) {
+  console.log(req.method, req.url)
   debug(req.method + ' ' + req.url)
 
   switch (true) {
@@ -27,15 +31,25 @@ var matchRoutes = function (req, res) {
   }
 }
 
-// Create app
+var templates = require('./templates')(viewsDir)
+// Create app, attach routes and sessions
 var app = connect()
   .use(function (req, res) {
     matchRoutes(req, res)
-  });
+  })
+  .use(connect.session({
+    store: new redis({
+      port: redisUrl.port,
+      host: redisUrl.hostname,
+      // pass: redisUrl.auth.split(":")[1]
+    }),
+    secret: process.env.SESSION_SECRET || 'woof woof'
+  }));
 
-var templates = require('./templates')(viewsDir)
 // Create server
 var server = app.listen(port, function (d,e) {
   debug('listening on port %s', port)
 })
+
+// Attach sockets to Connect server
 var chat = require('./chat')(server)
