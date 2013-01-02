69c7edf3cf223704a64771d10727ee2844b3aff8
Steve Klise <sk@skli.se>
2013-01-01 23:36:13 -0500

    Move Handlebars helpers to helpers.js

--- public/js/application.js
+++ public/js/application.js
@@ -1,13 +1,3 @@
-Handlebars.registerHelper('dateString', function (date) {
-  var d = new Date(date)
-  return (d.getMonth() + 1) + "/" + pad(d.getDate()) + " " + hours(d.getHours()) + ":" + pad(d.getMinutes())
-});
-
-Handlebars.registerHelper('timeString', function (timestamp) {
-  var d = new Date(timestamp)
-  return hours(d.getHours()) + ":" + pad(d.getMinutes())
-})
-
 $(document).ready(function () {
   app = window.app != null ? window.app : {}
   app.router = new app.ChannelRouter
--- public/js/helpers.js
+++ public/js/helpers.js
@@ -1,4 +1,14 @@
 (function() {
+  Handlebars.registerHelper('dateString', function (date) {
+    var d = new Date(date)
+    return (d.getMonth() + 1) + "/" + pad(d.getDate()) + " " + hours(d.getHours()) + ":" + pad(d.getMinutes())
+  });
+
+  Handlebars.registerHelper('timeString', function (timestamp) {
+    var d = new Date(timestamp)
+    return hours(d.getHours()) + ":" + pad(d.getMinutes())
+  })
+
   $(document).ready(function () {
     window.windowBlurred = false
     window.pageIsBlinking = false
