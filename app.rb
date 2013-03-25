9cef7ef0e8f67594594b50068f3ad971cc3cf1c6
Steve Klise <sk@skli.se>
2013-03-25 18:22:48 -0400

    Move most everything to app.rb

--- app.rb
+++ app.rb
@@ -1,4 +1,46 @@
+require 'bundler'
+Bundler.require
+
+require './models'
+
 class Shep < Sinatra::Base
+  use Rack::Session::Cookie, secret: ENV['SESSION_SECRET']
+  use Rack::Flash, accessorize: [:error, :success]
+
+  use Warden::Manager do |config|
+    config.serialize_into_session{|user| user.id }
+    config.serialize_from_session{|id| User.get(id) }
+
+    config.scope_defaults :default,
+      strategies: [:password],
+      action: 'auth/unauthenticated'
+    config.failure_app = self
+  end
+
+  Warden::Manager.before_failure do |env,opts|
+    env['REQUEST_METHOD'] = 'POST'
+  end
+
+  Warden::Strategies.add(:password) do
+    def valid?
+      params['user'] && params['user']['username'] && params['user']['password']
+    end
+
+    def authenticate!
+      user = User.first(username: params['user']['username'])
+
+      if user.nil?
+        fail!("The username you entered does not exist.")
+        flash.error = ""
+      elsif user.authenticate(params['user']['password'])
+        flash.success = "Successfully Logged In"
+        success!(user)
+      else
+        fail!("Could not log in")
+      end
+    end
+  end
+
   set :static, true
   set :logging, true
   set :cache, Dalli::Client.new
